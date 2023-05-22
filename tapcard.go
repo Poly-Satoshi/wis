25dc755c3fbc73e597f59c4728ad144a0e244867
Mylo Cyrus <irahiggins740@gmail.com>
2023-05-21 18:29:57 -0700

    Add files via upload

---
+++ tapcard.go
@@ -0,0 +1,36 @@
+Ride History
+Date / Time
+Type
+Status
+Product
+Device ID
+Debit
+Balance
+May 8, 2023 at 10:22 AM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08476	$0.00	$0.00
+May 8, 2023 at 9:37 AM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08491	$0.00	$0.00
+Apr 28, 2023 at 11:26 AM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08448	$0.00	$0.00
+Apr 26, 2023 at 12:55 PM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08439	$0.00	$0.00
+Apr 25, 2023 at 4:27 PM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC01749	$0.00	$0.00
+Apr 25, 2023 at 3:08 PM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08442	$0.00	$0.00
+Apr 23, 2023 at 12:42 PM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC01734	$0.00	$0.00
+Apr 23, 2023 at 9:34 AM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC05864	$0.00	$0.00
+Apr 21, 2023 at 1:28 PM	No Prod Use Type	Insufficient Value	Reg SV Regular	DCC08490	$0.00	$0.00
+Apr 18, 2023 at 2:48 PM	Pass Entry	Success	Metro Visitor 7	DCC08443	$0.00	$0.00
+Apr 18, 2023 at 12:23 PM	Pass Entry	Success	Metro Visitor 7	DCC08449	$0.00	$0.00
+Apr 18, 2023 at 12:00 PM	Pass Entry	Success	Metro Visitor 7	DCC02048	$0.00	$0.00
+Apr 18, 2023 at 10:55 AM	Pass Entry	Success	Metro Visitor 7	DCC08450	$0.00	$0.00
+Apr 16, 2023 at 2:04 PM	Pass Entry	Success	Metro Visitor 7	DCC08481	$0.00	$0.00
+Apr 16, 2023 at 11:18 AM	Pass Entry	Success	Metro Visitor 7	DCC08480	$0.00	$0.00
+Apr 16, 2023 at 10:02 AM	Pass Entry	Success	Metro Visitor 7	DCC08483	$0.00	$0.00
+Apr 14, 2023 at 3:41 PM	Pass Entry	Success	Metro Visitor 7	DCC08290	$0.00	$0.00
+Apr 12, 2023 at 3:45 PM	Pass Entry	Pass First Use	Metro Visitor 7	DCC08448	$0.00	$0.00
+Sale History
+Date / Time
+Type
+Status
+Product
+Sale Device ID
+Order Amount
+Nov 8, 2022 at 12:01 PM	Pass Sale	Success	Metro Visitor 7	MTL00686$0.00
+Sep 21, 2022 at 7:21 AM	Initialize	Success		POI00300-
+Sep 21, 2022 at 7:21 AM	Issue	Success	Reg SV Regular	POI00300
